f5368361c0d7d9eb63e19154e3b0f0d14d96121c
Niketan Gadade <niketangadade88@gmail.com>
2025-03-29 08:40:09 +0530

    added config themes

---
+++ movie_search_application/lib/core/configs/theme/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static const Color primary = Color.fromRGBO(212, 175, 55, 1);
+  static const Color secondary = Color.fromRGBO(41, 121, 255, 1);
+  static const Color background = Color.fromRGBO(0, 0, 0, 1);
+
+}
